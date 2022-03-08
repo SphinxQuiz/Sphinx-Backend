ad186669cd70b99770dab1beaae30e14efab77ab
thomas-mauran <thomas.mauran@etu.umontpellier.fr>
2022-03-08 08:32:23 +0100

    login middleware

--- controllers/user.js
+++ controllers/user.js
@@ -23,8 +23,25 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res.status(401); // a finir;
+        return res.status(401).json({ error: "This user does not exist" }); // a finir;
       }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Incorrect password" });
+          }
+          return res
+            .status(200)
+            .json({
+              userId: user._id,
+              token: jwt.sign({ userId: user._id }, "H6zT5kXx6Ffi6RI8rKSb", {
+                expiresIn: "24h",
+              }),
+            });
+        })
+
+        .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
